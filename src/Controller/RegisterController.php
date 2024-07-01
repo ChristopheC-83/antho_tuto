7ef699d960a951ee6ac9ee59a3bcb99a954dbd20
kikisan <kiketdule@gmail.com>
2024-07-01 11:12:23 +0200

    chiffrement mdp

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em,UserPasswordHasherInterface $passwordHashed): Response
     {
-        return $this->render('register/index.html.twig');
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // dd($passwordHashed->hashPassword($user,$form->getData()->getPassword()));
+            $user->setPassword($passwordHashed->hashPassword($user, $form->getData()->getPassword()));
+
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre compte a bien été créé ! Vous pouvez vous connecter.');
+            return $this->redirectToRoute('app_login');
+        } else {
+            $this->addFlash('alert', 'Echec de la création de votre compte.');
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use PharIo\Manifest\Email;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,24 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre adresse email',
+                ]                
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe',
+                ]
+            ])
+            ->add ('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => [
+                    'class' => 'btn-light mt-2',
+                ]
+            ])
         ;
     }
 
